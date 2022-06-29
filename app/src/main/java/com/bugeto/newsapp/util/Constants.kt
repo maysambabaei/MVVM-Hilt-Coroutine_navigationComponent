df8c3b5a79bcb2e5ff0a928d3e0199060ed168b8
Maysam Babaei <39745651+maysambabaei@users.noreply.github.com>
2022-06-29 16:49:17 +0430

    Update Constants.kt

--- app/src/main/java/com/bugeto/newsapp/util/Constants.kt
+++ app/src/main/java/com/bugeto/newsapp/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.bugeto.newsapp.util
 
-const val API_KEY = "95a4d6bb0f444945b1b3ca7698dafcf0"
+const val API_KEY = ""
 const val BASE_URL = "https://newsapi.org"
 const val SEARCH_NEWS_TIME_DELAY = 500L
 const val QUERY_PAGE_SIZE = 20
